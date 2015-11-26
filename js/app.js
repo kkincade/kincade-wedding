46e96582ae6fca254e7cb701a4f3f80d4cacfe86
Kameron Kincade <kameron.kincade@icloud.com>
2015-11-25 20:42:14 -0700

    Converted the JS into an object oriented approach.

--- js/app.js
+++ js/app.js
@@ -1,30 +1,47 @@
-$(function() {
+(function ($) {
     'use strict';
 
-    var controls = {
-        $doc: $(document),
-        $backgroundImage: $('.background-image'),
-        googleMap: document.getElementById('googleMap')
-    };          
+    var WebsiteControl = function (element, options) {
+        this.options = options;
+        this.$element = $(element);
+        this.$doc = $(document);
+        this.$body = $('body');
 
-    // Smooth scrolling
-    var hrefElements = $('a[href*=#]:not([href=#])');
-    hrefElements.click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-            
-            if (target.length) {
-                target.velocity('scroll', { duration: 1000, easing: 'easeOutExpo' });
+        this.init();
+    };
 
-                // Don't remember event in browser history
-                event.preventDefault();
-                return false;
+    WebsiteControl.prototype.init = function () {
+        this.controls = {
+            $backgroundImage: $('.background-image'),
+            $googleMap: this.$element.find('.google-map')
+        };
+
+        this.initSmoothScroll();
+        this.initGoogleMap();
+    }
+
+    WebsiteControl.prototype.initSmoothScroll = function () {
+        var hrefElements = $('a[href*=#]:not([href=#])');
+
+        hrefElements.click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                
+                if (target.length) {
+                    target.velocity('scroll', { duration: 1000, easing: 'easeOutExpo' });
+
+                    // Don't remember event in browser history
+                    event.preventDefault();
+                    return false;
+                }
             }
-        }
-    });
+        });
+    }
 
-    function initializeGoogleMap() {
+    WebsiteControl.prototype.initGoogleMap = function () {
+        var _this = this;
+
         var denverLatLong = {
             lat: 39.755874,
             lng: -104.994157
@@ -38,13 +55,13 @@ $(function() {
         };
 
         // Create map with custom style
-        var map = new google.maps.Map(controls.googleMap, mapProperties);
-        map.set('styles', [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}]);
+        this.googleMap = new google.maps.Map(this.controls.$googleMap[0], mapProperties);
+        this.googleMap.set('styles', [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}]);
         
         // Set marker on Coors Field
         var marker = new google.maps.Marker({
             position: denverLatLong,
-            map: map,
+            map: this.googleMap,
             title: 'Denver, CO'
         });
 
@@ -64,26 +81,48 @@ $(function() {
         }
 
         // Hide the controls initially
-        map.setOptions(mapControlsOff)
+        this.googleMap.setOptions(mapControlsOff)
 
         // Listener to add controls
-        google.maps.event.addDomListener(map.getDiv(), 'mouseover', function(e) {
+        google.maps.event.addDomListener(this.googleMap.getDiv(), 'mouseover', function(e) {
             e.cancelBubble = true;
-            if (!map.hover) {
-                map.hover = true;
-                map.setOptions(mapControlsOn);
+            if (!_this.googleMap.hover) {
+                _this.googleMap.hover = true;
+                _this.googleMap.setOptions(mapControlsOn);
             }
         });
 
         // Listener to remove controls
-        google.maps.event.addDomListener(document.getElementsByTagName('body')[0], 'mouseover', function(e) {
-            if (map.hover) {
-                map.setOptions(mapControlsOff);
-                map.hover = false;
+        google.maps.event.addDomListener(this.$body[0], 'mouseover', function(e) {
+            if (_this.googleMap.hover) {
+                _this.googleMap.setOptions(mapControlsOff);
+                _this.googleMap.hover = false;
             }
         });
     }
+    // -------- PLUGIN DEFINITION --------
 
-    // Wait for DOM to load before initalizing map
-    google.maps.event.addDomListener(window, 'load', initializeGoogleMap);
-});
+    $.fn.websiteControl = function (option) {
+        return this.each(function () {
+            var $this = $(this);
+            var data = $this.data('kincade.websitecontrol');
+            var options = $.extend({}, WebsiteControl.DEFAULTS, $this.data(), typeof option === 'object' && option);
+
+            if (!data) {
+                $this.data('kincade.websitecontrol', (data = new WebsiteControl(this, options)));
+            }
+
+            if (typeof option === 'string') {
+                data[option]();
+            }
+        });
+    };
+
+    $.fn.websiteControl.Constructor = WebsiteControl;
+
+    // Find the "two factor container" and initialize the plugin
+    $(document).ready(function () {
+        $('.website-control').websiteControl();
+    });
+
+})(jQuery);
