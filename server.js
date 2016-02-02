0c40c9c7b3a978993f9f601d374f50d3941a9eee
Kameron Kincade <kameron.kincade@icloud.com>
2016-02-01 23:04:58 -0700

    Changing port number to 80

--- server.js
+++ server.js
@@ -183,8 +183,8 @@ var validateRsvp = function (rsvpData, response) {
 }
 
 // Set up a server-side listener for our site
-var server = app.listen(80, function () {
+var server = app.listen(80, '107.170.211.204' function () {
     var host = server.address().address;
     var port = server.address().port;
-    console.log("Site listening at http://%s:%s", host, port)
+    console.log("Site listening at http://localhost:%s", port)
 });
